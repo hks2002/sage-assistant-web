a471e7bcff92da46f01c282e8abdb22a075fbbbd
Robert Huang <56649783@qq.com>
2023-11-17 11:19:44 +0800

    fix: 🐛lottie z-index

--- src/components/lottie/WaitInputLottie.vue
+++ src/components/lottie/WaitInputLottie.vue
@@ -2,13 +2,18 @@
 * @Author                : Robert Huang<56649783@qq.com>
 * @CreatedDate           : 2022-05-29 19:46:00
 * @LastEditors           : Robert Huang<56649783@qq.com>
-* @LastEditDate          : 2023-06-10 10:00:03
-* @FilePath              : src/components/lottie/WaitInputLottie.vue
+* @LastEditDate          : 2023-11-17 11:18:07
 * @CopyRight             : Dedienne Aerospace China ZhuHai
 -->
 
 <template>
-  <LottiePlayer :animationLink="'/json/waiting-input.json'" :height="400" :width="400" class="flex fixed-center" />
+  <LottiePlayer
+    :animationLink="'/json/waiting-input.json'"
+    :height="400"
+    :width="400"
+    class="flex fixed-center"
+    style="z-index: -999"
+  />
 </template>
 
 <script setup>
