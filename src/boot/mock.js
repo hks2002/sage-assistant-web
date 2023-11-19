16bd869386b0b74a9f485c852e583ef4a4509bef
Robert Huang <56649783@qq.com>
2023-11-19 21:03:50 +0800

    ci: ⚙️enable mock

--- src/boot/mock.js
+++ src/boot/mock.js
@@ -2,12 +2,12 @@
  * @Author                : Robert Huang<56649783@qq.com>                     *
  * @CreatedDate           : 2022-03-25 11:01:00                               *
  * @LastEditors           : Robert Huang<56649783@qq.com>                     *
- * @LastEditDate          : 2023-11-15 23:30:11                               *
+ * @LastEditDate          : 2023-11-17 12:19:11                               *
  * @CopyRight             : Dedienne Aerospace China ZhuHai                   *
  *****************************************************************************/
 
 // the mock index only works in DEV
 if (process.env.DEV) {
   console.debug('\u001b[35m' + '[Boot] ', 'Mock')
-  require('@/mock/index')
+  //require('@/mock/index')
 }
